5ae3231d7f738abd743f55637304172a50f9bfd6
elie-irani <elie.3irani@gmail.com>
2022-06-01 16:04:26 +0300

    Added mongodb driver

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,6 +41,12 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_URI', 'mongodb+srv://elieirani:14=00elhgJ@portfolio.rcxli.mongodb.net/test'),
+            'database' => 'Portfolio',
         ],
 
         'mysql' => [
